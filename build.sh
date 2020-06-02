84230e0eb629d06d1f8ce03d7a6f7ecb4603f9c7
Ruslan Ibragimov <ruslan@ibragimov.by>
2020-06-02 15:31:45 +0300

    #3 Structure book, fix asciidoctor warning

--- build.sh
+++ build.sh
@@ -2,11 +2,28 @@
 
 set -e
 
-docker run --rm -v "$(pwd)":/documents/ \
-  asciidoctor/docker-asciidoctor asciidoctor index.adoc --out-file=dist/index.html
+mkdir -p dist
 
+echo "Building html"
 docker run --rm -v "$(pwd)":/documents/ \
-  asciidoctor/docker-asciidoctor asciidoctor-pdf index.adoc --out-file=dist/social-architecture-ru.pdf
+  asciidoctor/docker-asciidoctor asciidoctor index.adoc \
+  --out-file=dist/index.html \
+  -a revdate="$(date "-I")" \
+  -a revnumber="#${TRAVIS_COMMIT:-$(git log --pretty=format:'%h' -n 1)}"
+cp cover.png dist/cover.png
 
+echo "Building pdf"
 docker run --rm -v "$(pwd)":/documents/ \
-  asciidoctor/docker-asciidoctor asciidoctor-epub3 index.adoc --out-file=dist/social-architecture-ru.epub
+  asciidoctor/docker-asciidoctor asciidoctor-pdf index.adoc \
+  --out-file=dist/social-architecture-ru.pdf \
+  -a revdate="$(date "-I")" \
+  -a revnumber="#${TRAVIS_COMMIT:-$(git log --pretty=format:'%h' -n 1)}"
+
+echo "Building epub"
+docker run --rm -v "$(pwd)":/documents/ \
+  asciidoctor/docker-asciidoctor asciidoctor-epub3 index.adoc \
+  --out-file=dist/social-architecture-ru.epub \
+  -a revdate="$(date "-I")" \
+  -a revnumber="#${TRAVIS_COMMIT:-$(git log --pretty=format:'%h' -n 1)}"
+
+echo "Building done"
